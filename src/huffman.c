577a1fdfec6cb30099b766d46f4262e2665feada
mariaedelgado <maria.delgado.tejedor@gmail.com>
2023-11-20 12:23:27 +0100

    feat: define method to compress file
    Definition of method to compress a file using Huffman method. Still WIP

--- src/huffman.c
+++ src/huffman.c
@@ -1,7 +1,15 @@
 #include "../include/huffman.h"
+#include "../include/symbols.h"
 
-void hello()
+int hufmann__compress_file(FILE* fh_in, FILE* fh_out)
 {
-    printf("Helloooooo");
-    return;
+    int ret = -1;
+    symbols_t symbols = { 0 };
+
+    // First, the input file must be parsed and every character will be stored in
+    // a symbol_t struct
+    ret = symbols__get_from_input_file(&symbols, fh_in);
+    symbols__print(&symbols);
+
+    return ret;
 }
