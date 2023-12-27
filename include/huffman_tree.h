96829d07758b8e61c72da83a89f0c77e4cf3cdcf
mariaedelgado <maria.delgado.tejedor@gmail.com>
2023-12-27 09:13:00 +0100

    doc: add comments to huffman_tree methods

--- include/huffman_tree.h
+++ include/huffman_tree.h
@@ -13,7 +13,14 @@ typedef struct {
     int rchild_index;
 } node_t;
 
+/** \brief Asserts if input node is a leaf.
+ * \param node as const node_t*. Pointer to the node that we want to assert.
+ * \return bool. 'true' if leaf node, 'false' if intermediate node.
+ * 
+*/
 bool is_leaf(const node_t* node);
+
+/* ---------------------------------------------------------------------------------- */
 
 typedef struct {
 
@@ -22,10 +29,31 @@ typedef struct {
 
 } huffman_tree_t;
 
+/** \brief Stores character in the Huffman Tree (as a leaf node, since this method)
+ * is to be called when reading an input file to encode.
+ * \param self as huffman_tree_t*. Pointer to the Huffman Tree to be filled.
+ * \param character as char. Character to be added in the Huffman Tree as a leaf.
+ * If the character is already present, its frequency will go up by one unit.
+ * \return int. Error code (0 if OK). 
+*/
 int huffman_tree__store_character(huffman_tree_t* self, char character);
 
+/** Sorts the Huffman Tree once all the file has been read and all the characters
+ * have been added, in ascending order (by frequency).
+ * \param self as huffman_tree_t*. Pointer to the Huffman Tree to be sorted.
+ * \return int. Error code (0 if OK).
+*/
 int huffman_tree__sort(huffman_tree_t* self);
 
+/** \brief Generates the Huffman Tree from the leaf nodes, creating the necessary
+ * intermediate nodes to complete it.
+ * \param self as huffman_tree_t*. Pointer to the Huffman Tree.
+ * \return int. Error code (0 if OK).
+*/
 int huffman_tree__generate(huffman_tree_t* self);
 
+/** \brief Destroyer of the Huffman Tree instance.
+ * \param self as huffman_tree_t*. Pointer to the Huffman Tree.
+ * \return int. Error code (0 if OK).
+*/
 int huffman_tree__destroy(huffman_tree_t* self);
