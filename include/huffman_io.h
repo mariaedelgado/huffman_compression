79a66e0a67d43a075f55d13f02af8579bc6e3a15
mariaedelgado <maria.delgado.tejedor@gmail.com>
2023-12-27 09:22:29 +0100

    feat: add hufman_io__write
    Add method to write the Huffman encoded file into a file.

--- include/huffman_io.h
+++ include/huffman_io.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "huffman_tree.h"
+#include "huffman_codes.h"
 
 typedef struct {
     FILE* fh_in;
@@ -16,3 +16,13 @@ typedef struct {
 * \return int. Error code (0 if OK).
 */
 int huffman_io__read(huffman_io_t* self, huffman_tree_t* huffman_tree);
+
+/** \brief Write encoded output using the Huffman codes generated from the tree.
+ * \param self as huffman_io_t*. Pointer to the huffman_io_t struct.
+ * \param huffman_tree as huffman_tree_t*. Huffman tree to be added in the encoded
+ * file so that we can decode the file later.
+ * \param huffman_codes as huffman_codes_t*. Table containing the ASCII printable
+ * characters and the equivalent Huffman code for each.
+ * \return int. Error code (0 if OK).
+*/
+int huffman_io__write(huffman_io_t* self, huffman_tree_t* huffman_tree, huffman_codes_t* huffman_codes);
--- src/huffman_io.c
+++ src/huffman_io.c
@@ -34,3 +34,85 @@ int huffman_io__read(huffman_io_t* self, huffman_tree_t* huffman_tree)
 end:
     return ret;
 }
+
+/* --------------------------------------------------------------------------------------------------- */
+
+static int huffman_io__encode_node(huffman_io_t* self, huffman_tree_t* huffman_tree, int current_index)
+{
+    int ret = 0;
+
+    if ((self == NULL) || (huffman_tree == NULL)) {
+        ret = -1;
+        goto end;
+    }
+
+    node_t* current_node = &huffman_tree->node[current_index];
+
+    if (is_leaf(current_node)) {
+        fprintf(self->fh_out, "1");
+        fprintf(self->fh_out, "%c", current_node->character);
+
+    } else {
+        fprintf(self->fh_out, "0");
+        ret = huffman_io__encode_node(self, huffman_tree, current_node->lchild_index);
+        ret = huffman_io__encode_node(self, huffman_tree, current_node->rchild_index);
+    }
+
+end:
+    return ret;
+}
+
+static int huffman_io__write_header(huffman_io_t* self, huffman_tree_t* huffman_tree)
+{
+    int ret = -1;
+
+    if ((self->fh_out == NULL) || (huffman_tree == NULL)) {
+        goto end;
+    }
+
+    // Explain Huffman tree encoding method !!!!!!!!!!!!!!!!!
+    // Starting at root node
+    ret = huffman_io__encode_node(self, huffman_tree, huffman_tree->number_of_nodes - 1);
+
+end:
+    return ret;
+}
+
+int huffman_io__write(huffman_io_t* self, huffman_tree_t* huffman_tree, huffman_codes_t* huffman_codes)
+{
+    int ret = -1;
+
+    if ((self->fh_in == NULL) || (self->fh_out == NULL)) {
+        goto end;
+    }
+
+    // First, write the header (containing the Huffman tree)
+    ret = huffman_io__write_header(self, huffman_tree);
+
+    // Rewind fh_in, since it has already been read
+    rewind(self->fh_in);
+
+    // Read the file character by character and print the respective Huffman code for the
+    // given character in the output file. Write a space between the encoded header and the
+    // encoded bytes so that we can distinguish between the Huffman Tree and the file itself.
+    char c = ' ';
+    int pos = 0;
+    fprintf(self->fh_out, " ");
+
+    while (true)
+    {
+        c = fgetc(self->fh_in);
+
+        if (feof(self->fh_in)) {
+            ret = 0;
+            break;
+        }
+
+        // Since the character is at the same time the index in our huffman_code_t table (when substracted 32).
+        pos = (int)c - 32;
+        fprintf(self->fh_out, "%.*s", huffman_codes->n_significant_bits[pos], huffman_codes->code[pos]);
+    }
+
+end:
+    return ret;
+}
